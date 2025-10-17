a4b32ff57f6c47a6877f2bdf452614381907d1e1
Maximo Faust <maxifr7@gmail.com>
2025-10-17 08:51:39 -0300

    Proyecto inicial generado con Spring Initializr

---
+++ src/main/java/um/edu/demospringum/DemospringumApplication.java
@@ -0,0 +1,13 @@
+package um.edu.demospringum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/um/edu/demospringum/DemospringumApplicationTests.java
@@ -0,0 +1,13 @@
+package um.edu.demospringum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
