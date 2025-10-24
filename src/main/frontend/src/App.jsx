d2506551d4fde6afd66c1692bb9485e85f74df4c
andres <abellini@correo.um.edu.uy>
2025-10-24 08:44:29 -0300

    Agrega una demo de la pagina de personalización

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
-import Navbar from './components/Navbar.jsx'
 import HeroSection from './components/HeroSection.jsx'
 import ProductosDestacados from './components/ProductosDestacados.jsx'
-import FeaturesSection from "./components/FeaturesSection.jsx";
-
+import FeaturesSection from "./components/FeaturesSection.jsx"
 
 export default function App(){
-  return (
-    <>
-      <Navbar />
-      <main>
-        <HeroSection />
-          <FeaturesSection />
-
-        <ProductosDestacados />
-      </main>
-      <footer className="py-4 text-center text-sm text-neutral-500">
-        © {new Date().getFullYear()} JAM Consultancy & Services | Todos los derechos reservados
-      </footer>
-    </>
-  )
+    return (
+        <main>
+            <HeroSection />
+            <FeaturesSection />
+            <ProductosDestacados />
+        </main>
+    )
 }
